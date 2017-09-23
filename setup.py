b69402c40ecb1dbeb314602477ecbf6af03e33c5
Helge Spieker <helge.spieker@inf.h-brs.de>
2017-09-23 19:04:58 +0200

    Initial setup.py for easier package installation and distribution.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='scihub.py',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/zaytoun/scihub.py',
+    license='MIT',
+    author='zaytoun',
+    author_email='',
+    description='Python API and command-line tool for Sci-Hub'
+)
